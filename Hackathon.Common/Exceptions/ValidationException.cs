3f704243dce42da30ffcc2a33f21b6a1f05129cc
coder1coder <13835906+coder1coder@users.noreply.github.com>
2021-11-26 20:29:29 +0300

    Добавил глобальный обработчик исключений с использованием ProblemDetails (#43)

---
+++ Hackathon.Common/Exceptions/ValidationException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Hackathon.Common.Exceptions
+{
+    public class ValidationException: Exception
+    {
+        public ValidationException(string message, Exception exception = null) : base(message, exception)
+        {
+        }
+    }
+}
