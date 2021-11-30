60b1ca6e8fcb9b640256ee5b4503e998ecc81756
Mikhail Coder <coder1coder@gmail.com>
2021-11-30 22:32:38 +0300

    Валидатор модели создания события

--- Hackathon.BL/Event/Validators/CreateEventModelValidator.cs
+++ Hackathon.BL/Event/Validators/CreateEventModelValidator.cs
@@ -1,6 +1,7 @@
 ﻿using FluentValidation;
 using Hackathon.Common.Models;
 using System;
+using System.Linq;
 using Hackathon.Common.Models.Event;
 
 namespace Hackathon.BL.Event.Validators
@@ -10,15 +11,49 @@ namespace Hackathon.BL.Event.Validators
         public CreateEventModelValidator()
         {
             RuleFor(x => x.Name)
-                .NotNull()
                 .NotEmpty()
                 .MinimumLength(5)
-                .MaximumLength(100); 
-            
+                .MaximumLength(100);
+
             RuleFor(x => x.Start)
-               .NotNull()
-               .NotEmpty()
-               .GreaterThan(DateTime.UtcNow).WithMessage("Значение должно быть больше текущих даты и времени");
+               .GreaterThan(DateTime.UtcNow)
+               .WithMessage("Дата начала мероприятия должна быть позже текущего времени");
+
+            RuleFor(x => x.DevelopmentMinutes)
+                .GreaterThan(0)
+                .WithMessage("Продолжительность этапа разработки должна быть больше 0 минут");
+
+            RuleFor(x => x.MaxEventMembers)
+                .GreaterThan(1)
+                .WithMessage("Максимальное количество участников события должно быть больше 1");
+
+            RuleFor(x => x.MemberRegistrationMinutes)
+                .GreaterThan(0)
+                .WithMessage("Продолжительность этапа регистрации участников должна быть больше 0 минут");
+
+            RuleFor(x => x.MinTeamMembers)
+                .GreaterThan(0)
+                .WithMessage("Минимальное количество участников в команде должно быть больше 0");
+
+            RuleFor(x => x.TeamPresentationMinutes)
+                .GreaterThan(0)
+                .WithMessage("Продолжительность этапа презентации должна быть больше 0 минут");
+
+            When(x => x.ChangeEventStatusMessages.Any(), () =>
+            {
+                RuleForEach(x => x.ChangeEventStatusMessages).ChildRules(statusMessages =>
+                {
+                    statusMessages
+                        .RuleFor(x => x.Message)
+                        .NotEmpty()
+                        .WithMessage("Указаны не существующие статусы события");
+
+                    statusMessages
+                        .RuleFor(x => x.Status)
+                        .IsInEnum()
+                        .WithMessage("Нельзя указывать пустые сообщения для рассылки");
+                });
+            });
         }
     }
 }
