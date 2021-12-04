1006f6b9d7ff93ab0e2917423ece1d4f8c2b7e77
Mikhail Coder <coder1coder@gmail.com>
2021-12-04 13:37:46 +0300

    use 7100 for tests

--- Hackathon.Tests/Hackathon.Tests.Integration/TestWebApplicationFactory.cs
+++ Hackathon.Tests/Hackathon.Tests.Integration/TestWebApplicationFactory.cs
@@ -1,9 +1,15 @@
 ﻿using Hackathon.API;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 
 namespace Hackathon.Tests.Integration
 {
     public class TestWebApplicationFactory : WebApplicationFactory<Startup>
     {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            base.ConfigureWebHost(builder);
+            builder.ConfigureKestrel(x => x.ListenLocalhost(7100));
+        }
     }
 }
