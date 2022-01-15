e2188d284544b453dba851b7b1961ebff437ebaf
kalilask4 <kalilask4@gmail.com>
2022-01-15 03:56:10 +0300

    Добавлен контроллер для авторизации администратора

---
+++ Hackathon.API/Abstraction/IAdminApi.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using Hackathon.Common.Models;
+using Hackathon.Contracts.Requests.User;
+using Refit;
+
+namespace Hackathon.API.Abstraction
+{
+    public interface IAdminApi
+    {
+        [Post($"/api/Admin/{nameof(SignIn)}")]
+        public Task<AuthTokenModel> SignIn([Body] SignInRequest request);
+    }
+}
