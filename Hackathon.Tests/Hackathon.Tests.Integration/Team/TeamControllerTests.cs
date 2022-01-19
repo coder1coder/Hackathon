5c006a5865dd9e029bf6fda37ee661ea8b6ca36e
kalilask4 <kalilask4@gmail.com>
2022-01-19 18:54:12 +0300

    Удален неактуальный тест

--- Hackathon.Tests/Hackathon.Tests.Integration/Team/TeamControllerTests.cs
+++ Hackathon.Tests/Hackathon.Tests.Integration/Team/TeamControllerTests.cs
@@ -43,39 +43,5 @@ namespace Hackathon.Tests.Integration.Team
 
             teamExist.Should().BeTrue();
         }
-
-      /*  [Fact]
-        public async Task AddMember_Should_Success()
-        {
-            var eventModel = TestFaker.GetEventModels(1, UserId).First();
-            var eventRequest = Mapper.Map<CreateEventRequest>(eventModel);
-            var createEventResponse = await EventsApi.Create(eventRequest);
-
-            await EventsApi.SetStatus(new SetStatusRequest<EventStatus>
-            {
-                Id = createEventResponse.Id,
-                Status = EventStatus.Published
-            });
-
-            var teamCreateResponse = await TeamsApi.Create(new CreateTeamRequest
-            {
-                Name = Guid.NewGuid().ToString()[..4],
-                EventId = createEventResponse.Id
-            });
-
-            var fakeRequest = Mapper.Map<SignUpRequest>(TestFaker.GetSignUpModels(1).First());
-
-            var createUserResponse = await UsersApi.SignUp(fakeRequest);
-
-            await FluentActions.Invoking(async () =>
-                {
-                    await TeamsApi.AddMember(new TeamAddMemberRequest
-                    {
-                        TeamId = teamCreateResponse.Id,
-                        UserId = createUserResponse.Id
-                    });
-                })
-                .Should().NotThrowAsync<Exception>();
-        }*/
     }
 }
