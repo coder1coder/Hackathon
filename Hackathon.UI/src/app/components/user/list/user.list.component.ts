34eafa8417759faad3397e6b43961c2e45b61f1f
kalilask4 <kalilask4@gmail.com>
2022-01-20 06:00:23 +0300

    Добавлен компонент userList

---
+++ Hackathon.UI/src/app/components/user/list/user.list.component.ts
@@ -0,0 +1,67 @@
+import {AfterViewInit, Component} from '@angular/core';
+import {Router} from "@angular/router";
+import {PageSettings, PageSettingsDefaults} from "../../../models/PageSettings";
+import {UserService} from "../../../services/user.service";
+import {UserModel} from "../../../models/User/UserModel";
+import {BaseCollectionModel} from "../../../models/BaseCollectionModel";
+import {PageUserSettings, PageUserSettingsDefaults} from "../../../models/PageUserSettings";
+import {PageUser} from "@angular/material/paginator";
+//import {PageUser} from "@angular/core";
+import {PageEvent} from "@angular/material/paginator";
+
+@Component({
+  selector: 'user-list',
+  //template: `<h2>user list</h2>`,
+  templateUrl: './user.list.component.html',
+  styleUrls: ['./user.list.component.scss']
+})
+export class UserListComponent implements AfterViewInit {
+
+  users: UserModel[] = [];
+  displayedColumns: string[] = ['id', 'userName', 'email', 'fullName', 'actions'];
+  pageSettings: PageUser = new PageUser();
+
+  constructor(private usersService: UserService, private router: Router) {
+
+    let pageSettingsJson = sessionStorage.getItem(`${UserListComponent.name}${PageUser.name}`);
+
+
+    if (pageSettingsJson != null)
+      this.pageSettings = JSON.parse(pageSettingsJson)
+    else
+    {
+      this.pageSettings.pageSize = PageSettingsDefaults.PageSize;
+      this.pageSettings.pageIndex = PageSettingsDefaults.PageIndex;
+    }
+  }
+
+  setPageSettings(user:PageUser){
+    this.pageSettings = user;
+    sessionStorage.setItem(`${UserListComponent.name}${PageUser.name}`, JSON.stringify(user));
+
+    this.fetch();
+  }
+
+  ngAfterViewInit(): void {
+    this.fetch();
+  }
+
+  fetch(){
+    this.usersService.getAll(new PageUserSettings(this.pageUserSettings))
+      .subscribe({
+        next: (r: BaseCollectionModel<UserModel>) =>  {
+          this.users = r.items;
+          this.pageSettings.length = r.totalCount;
+        },
+        error: () => {}
+      });
+  }
+
+  handleRowClick(user: UserModel){
+    this.router.navigate(['/users/'+user.id]);
+  }
+
+  // showUserPage(user: UserModel){
+  //   this.router.navigate(['/user/'+user.id]);
+  // }
+}
