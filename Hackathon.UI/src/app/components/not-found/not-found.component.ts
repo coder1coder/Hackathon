db8a8d544051de866132346cbf7da38c2126bb78
Mikhail Coder <coder1coder@gmail.com>
2022-01-23 00:20:00 +0300

    Добавлена страница 404

--- Hackathon.UI/src/app/components/not-found/not-found.component.ts
+++ Hackathon.UI/src/app/components/not-found/not-found.component.ts
@@ -4,10 +4,12 @@ import {Router} from "@angular/router";
 @Component({
   selector: 'not-found',
   templateUrl: './not-found.component.html',
-  styleUrls: ['./not-found.component.scss']
+  styleUrls: ['./not-found.component.scss'],
 })
 
-export class NotFoundComponent{
+export class NotFoundComponent {
+
   constructor(private router: Router) {
   }
+  
 }
