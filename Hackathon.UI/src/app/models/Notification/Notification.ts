a05d8ccd55f7f8fe008b9287eb3067d7001ff26b
coder1coder <13835906+coder1coder@users.noreply.github.com>
2022-02-24 20:57:23 +0300

    Получить список уведомлений (#122)

--- Hackathon.UI/src/app/models/Notification/Notification.ts
+++ Hackathon.UI/src/app/models/Notification/Notification.ts
@@ -1,6 +1,19 @@
+import {NotificationType} from "./NotificationType";
+
 export class Notification
 {
   public static getParsedData(data:any): any {
     return JSON.parse(data);
+  }
+
+  public static getTypeName(type:NotificationType){
+    switch (type){
+      case NotificationType.Information:
+        return 'Информация';
+      case NotificationType.TeamInvite:
+        return 'Приглашение в команду';
+      case NotificationType.EventInvite:
+        return 'Приглашение в событие';
+    }
   }
 }
