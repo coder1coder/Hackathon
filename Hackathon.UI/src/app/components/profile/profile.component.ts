750a84ebff925bb1ea3cf9cfa48f55eea08c5f42
Evgenii Solovev <AshtonBro@github.com>
2022-04-23 02:09:34 +0500

    Добавлен метод загрузки файла на странице профиля

--- Hackathon.UI/src/app/components/profile/profile.component.ts
+++ Hackathon.UI/src/app/components/profile/profile.component.ts
@@ -1,53 +1,86 @@
-import {AfterViewInit, Component} from "@angular/core";
-import {AuthService} from "../../services/auth.service";
-import {UserModel} from "../../models/User/UserModel";
-import {FormControl, FormGroup} from "@angular/forms";
+import { AfterViewInit, Component, OnInit } from "@angular/core";
+import { AuthService } from "../../services/auth.service";
+import { UserModel } from "../../models/User/UserModel";
+import { FormBuilder, FormGroup } from "@angular/forms";
 import { UserRoleTranslator } from "src/app/models/User/UserRole";
+import { UserService } from "src/app/services/user.service";
+import { DomSanitizer } from "@angular/platform-browser";
+import { map } from "rxjs";
 
 @Component({
   templateUrl: './profile.component.html',
   styleUrls:['./profile.component.scss']
 })
-export class ProfileComponent implements AfterViewInit {
+export class ProfileComponent implements OnInit, AfterViewInit {
+  public image: any;
+  public form = new FormGroup({})
 
-  currentUser:UserModel = new UserModel();
-  UserRoleTranslator = UserRoleTranslator;
+  private currentUser: UserModel = new UserModel();
+  private UserRoleTranslator: UserRoleTranslator = UserRoleTranslator;
 
-  form = new FormGroup({
-    id: new FormControl(),
-    fullName: new FormControl(),
-    email: new FormControl(),
-    userName: new FormControl(),
-    role: new FormControl(),
-  })
+  constructor(
+    private authService: AuthService,
+    private fb: FormBuilder,
+    private userService: UserService,
+    private sanitazer: DomSanitizer
+    ) {
+  }
 
-  constructor(private authService: AuthService) {
+  ngOnInit(): void {
+    this.initForm();
   }
 
   ngAfterViewInit(): void {
-
-    let request = this.authService.getCurrentUser();
-
-    if (request != undefined)
-    {
-      request.subscribe({
-        next: r=> {
-          this.currentUser = r;
-
-          this.form = new FormGroup({
-            id: new FormControl(r.id),
-            fullName: new FormControl(r.fullName),
-            email: new FormControl(r.email),
-            userName: new FormControl(r.userName),
-            role: new FormControl(UserRoleTranslator.Translate(r.role)),
-          })
-
-        }
+    this.authService.getCurrentUser()
+      ?.subscribe((user: UserModel) => {
+        this.currentUser = user;
+        this.loadForm();
       })
-    }
+
+    // TODO Реализовать кнопку загрузки аватара (файла),
+    // TODO хранить id файла в userModel,
+    // TODO при инициализации компонента передавать fileId user в getProfileImage();
+    const fileId = 'f2a4d265-e8d5-4970-8b68-1f73fa8276ec';
+
+    this.userService.getProfileImage(fileId)
+      .pipe(
+        map((res: ArrayBuffer) => {
+          let TYPED_ARRAY = new Uint8Array(res);
+          const STRING_CHAR = TYPED_ARRAY.reduce((data, byte)=> {
+            return data + String.fromCharCode(byte);
+          }, '');
+          let base64String = btoa(STRING_CHAR);
+          return this.sanitazer.bypassSecurityTrustUrl('data:image/jpg;base64,' + base64String);
+        })
+      )
+      .subscribe(response => {
+        this.image = response;
+      });
   }
 
-  submit(){
+  public submit(): void {
 
+  }
+
+  private initForm(): void {
+    this.form = this.fb.group({
+      id: [null],
+      fullName: [null],
+      email: [null],
+      userName: [null],
+      role: [null],
+    });
+  }
+
+  private loadForm(): void {
+    if(this.currentUser) {
+      this.form.patchValue({
+        id: this.currentUser?.id,
+        fullName: this.currentUser?.fullName,
+        email: this.currentUser?.email,
+        userName: this.currentUser?.userName,
+        role: this.currentUser?.role,
+       });
+    }
   }
 }
--- Hackathon.UI/src/app/services/auth.service.ts
+++ Hackathon.UI/src/app/services/auth.service.ts
@@ -1,32 +1,33 @@
-import {HttpClient, HttpHeaders} from '@angular/common/http';
-import {EventEmitter, Injectable} from '@angular/core';
-import {Observable} from 'rxjs/internal/Observable';
-import {environment} from "../../environments/environment";
-import {map} from "rxjs/operators";
-import {CreateUser} from '../models/CreateUser';
-import {GetTokenResponse} from "../models/GetTokenResponse";
-import {BaseCreateResponse} from "../models/BaseCreateResponse";
-import {UserModel} from "../models/User/UserModel";
-import {AuthConstants} from "./auth.constants";
-import {GoogleSigninService} from './google-signin.service';
-import {GoogleUserModel} from '../models/User/GoogleUserModel';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { EventEmitter, Injectable } from '@angular/core';
+import { Observable } from 'rxjs/internal/Observable';
+import { environment } from "../../environments/environment";
+import { map } from "rxjs/operators";
+import { CreateUser } from '../models/CreateUser';
+import { GetTokenResponse } from "../models/GetTokenResponse";
+import { BaseCreateResponse } from "../models/BaseCreateResponse";
+import { UserModel } from "../models/User/UserModel";
+import { AuthConstants } from "./auth.constants";
+import { GoogleSigninService } from './google-signin.service';
+import { GoogleUserModel } from '../models/User/GoogleUserModel';
 
 @Injectable({
   providedIn: 'root'
 })
 
 export class AuthService {
+  public authChange: EventEmitter<boolean> = new EventEmitter();
 
-  private api = environment.api;
-  private storage = sessionStorage;
+  private api: string = environment.api;
+  private storage: Storage = sessionStorage;
 
-  authChange: EventEmitter<boolean> = new EventEmitter();
-
-  constructor(private http: HttpClient,
-    private googleSigninService: GoogleSigninService) {
+  constructor(
+    private http: HttpClient,
+    private googleSigninService: GoogleSigninService
+    ) {
   }
 
-  public isLoggedIn() : boolean {
+  public isLoggedIn(): boolean {
     let tokenInfo = this.getTokenInfo();
     if (tokenInfo == undefined)
       return false;
@@ -34,16 +35,7 @@ export class AuthService {
     return tokenInfo.expires >= Date.now();
   }
 
-  private getTokenInfo() : GetTokenResponse | undefined {
-    let authInfo = this.storage.getItem(AuthConstants.STORAGE_AUTH_KEY);
-    if (authInfo == undefined)
-      return undefined;
-
-    let auth:GetTokenResponse = JSON.parse(authInfo);
-    return auth;
-  }
-
-  public login(login: string, password: string) : Observable<GetTokenResponse> {
+  public login(login: string, password: string): Observable<GetTokenResponse> {
     return this.http.post<GetTokenResponse>(this.api+"/Auth/SignIn", {username: login, password: password})
       .pipe(map(r=>{
         this.storage.setItem(AuthConstants.STORAGE_AUTH_KEY, JSON.stringify(r));
@@ -52,7 +44,7 @@ export class AuthService {
       }));
   }
 
-  register(createUser:CreateUser) : Observable<BaseCreateResponse> {
+  public register(createUser:CreateUser): Observable<BaseCreateResponse> {
     return this.http.post<BaseCreateResponse>(this.api+"/User", {
       userName: createUser.userName,
       password: createUser.password,
@@ -61,18 +53,18 @@ export class AuthService {
     })
   }
 
-  public logout() : void {
+  public logout(): void {
     this.storage.removeItem(AuthConstants.STORAGE_AUTH_KEY);
     this.storage.removeItem(AuthConstants.STORAGE_USER_KEY);
     this.authChange.emit(false);
   }
 
-  public getUserId() : number | undefined {
+  public getUserId(): number | undefined {
     let tokenInfo = this.getTokenInfo();
     return tokenInfo?.userId;
   }
 
-  public getCurrentUser() : Observable<UserModel> | null {
+  public getCurrentUser(): Observable<UserModel> | null {
     if (!this.isLoggedIn())
       return null;
 
@@ -107,13 +99,22 @@ export class AuthService {
       }));
   }
 
-  public signInByGoogle() : Promise<void | gapi.auth2.GoogleUser> {
+  public signInByGoogle(): Promise<void | gapi.auth2.GoogleUser> {
     return this.googleSigninService.signIn();
   }
 
-  public signOutByGoogle() : any {
+  public signOutByGoogle(): any {
     this.storage.removeItem(AuthConstants.STORAGE_AUTH_KEY);
     this.storage.removeItem(AuthConstants.STORAGE_USER_KEY);
     return this.googleSigninService.signOut();
+  }
+
+  private getTokenInfo() : GetTokenResponse | undefined {
+    let authInfo = this.storage.getItem(AuthConstants.STORAGE_AUTH_KEY);
+    if (authInfo == undefined)
+      return undefined;
+
+    let auth:GetTokenResponse = JSON.parse(authInfo);
+    return auth;
   }
 }
