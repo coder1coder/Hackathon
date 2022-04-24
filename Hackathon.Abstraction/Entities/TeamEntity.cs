911759a66959657bef515acfbce5ef6700bfd7ac
Mikhail Coder <coder1coder@gmail.com>
2022-04-24 12:16:23 +0300

    В списке команд отображаются только команды созданные пользователями

--- Hackathon.Abstraction/Entities/TeamEntity.cs
+++ Hackathon.Abstraction/Entities/TeamEntity.cs
@@ -2,13 +2,34 @@
 
 namespace Hackathon.Abstraction.Entities
 {
+    /// <summary>
+    /// Команда
+    /// </summary>
     public class TeamEntity : BaseEntity
     {
+        /// <summary>
+        /// Наименование
+        /// </summary>
         public string? Name { get; set; }
 
+        /// <summary>
+        /// События
+        /// </summary>
         public List<TeamEventEntity> TeamEvents { get; set; } = new ();
+        
+        /// <summary>
+        /// Участники
+        /// </summary>
         public List<UserEntity> Users { get; set; } = new ();
+        
+        /// <summary>
+        /// Владелец команды
+        /// </summary>
         public UserEntity? Owner { get; set; }
+        
+        /// <summary>
+        /// Идентификатор владельца команды
+        /// </summary>
         public long? OwnerId { get; set; }
     }
 }
