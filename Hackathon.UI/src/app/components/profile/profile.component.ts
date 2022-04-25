0c3ca43c4dd75b5b278dddb226ddafd6e481bcd8
coder1coder <13835906+coder1coder@users.noreply.github.com>
2022-04-25 10:52:16 +0300

    Пример как загружать файлы (#158)
    В AuthInterceptor убрали из заголовков  'Content-Type': 'application/json' на каждый http запрос.

--- Hackathon.UI/src/app/components/profile/profile.component.ts
+++ Hackathon.UI/src/app/components/profile/profile.component.ts
@@ -1,14 +1,19 @@
-import {AfterViewInit, Component} from "@angular/core";
+import {AfterViewInit, Component, ViewChild} from "@angular/core";
 import {AuthService} from "../../services/auth.service";
 import {UserModel} from "../../models/User/UserModel";
 import {FormControl, FormGroup} from "@angular/forms";
 import { UserRoleTranslator } from "src/app/models/User/UserRole";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {environment} from "../../../environments/environment";
+import {Bucket} from "../../common/Bucket";
 
 @Component({
   templateUrl: './profile.component.html',
   styleUrls:['./profile.component.scss']
 })
 export class ProfileComponent implements AfterViewInit {
+
+  api = environment.api;
 
   currentUser:UserModel = new UserModel();
   UserRoleTranslator = UserRoleTranslator;
@@ -21,7 +26,9 @@ export class ProfileComponent implements AfterViewInit {
     role: new FormControl(),
   })
 
-  constructor(private authService: AuthService) {
+  @ViewChild('selectedFile') selectedFile: HTMLInputElement | undefined;
+
+  constructor(private authService: AuthService, private http:HttpClient) {
   }
 
   ngAfterViewInit(): void {
@@ -49,5 +56,22 @@ export class ProfileComponent implements AfterViewInit {
 
   submit(){
 
+  }
+
+  selectFile(event:any) {
+
+    if (event?.target?.files?.length > 0)
+    {
+      let file = event.target.files[0];
+
+      const formData = new FormData();
+      formData.append("file", file);
+
+      this.http.post(this.api+'/FileStorage/Upload/'+Bucket.Avatars, formData, {
+        headers: new HttpHeaders().set('Content-Disposition', 'multipart/form-data')
+      }).subscribe(x=>{
+        console.log(x)
+      })
+    }
   }
 }
