c6df67d0325ae01dcc6d0d5e0f5c6610635d234e
Evgenii Solovev <AshtonBro@github.com>
2022-05-07 20:28:19 +0500

    Task-165 Кастомное модальное окно

--- Hackathon.UI/src/app/services/snack.service.ts
+++ Hackathon.UI/src/app/services/snack.service.ts
@@ -1,7 +1,7 @@
 import {Injectable} from '@angular/core';
 import {environment} from "../../environments/environment";
-import {Actions} from "../common/Actions";
 import {MatSnackBar} from "@angular/material/snack-bar";
+import {Actions} from "../common/Actions";
 
 @Injectable({
   providedIn: 'root'
