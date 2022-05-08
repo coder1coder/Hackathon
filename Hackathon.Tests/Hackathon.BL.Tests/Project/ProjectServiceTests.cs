3dbf6e22ce7f759c9391151989523feba5409936
kalilask4 <kalilask4@gmail.com>
2022-05-08 23:11:45 +0300

    Рефакторинг

--- Hackathon.Tests/Hackathon.BL.Tests/Project/ProjectServiceTests.cs
+++ Hackathon.Tests/Hackathon.BL.Tests/Project/ProjectServiceTests.cs
@@ -17,17 +17,20 @@ namespace Hackathon.BL.Tests.Project;
 
 public class ProjectServiceTests: BaseUnitTest
 {
-    private Mock<IValidator<ProjectCreateModel>> _createValidatorMock = new();
-    private Mock<IProjectRepository> _projectRepositoryMock = new();
+    private Mock<IValidator<ProjectCreateModel>> _createValidatorMock;
+    private Mock<IProjectRepository> _projectRepositoryMock;
+
+    public ProjectServiceTests()
+    {
+        _createValidatorMock = new Mock<IValidator<ProjectCreateModel>>();
+        _projectRepositoryMock = new Mock<IProjectRepository>();
+    }
     
     [Fact]
     public async Task Create_Should_Return_Positive_Id()
     {
         //arrange
         var createdId = new Random().Next(0, 11);
-        
-        _createValidatorMock = new Mock<IValidator<ProjectCreateModel>>();
-        _projectRepositoryMock = new Mock<IProjectRepository>();
         
         _projectRepositoryMock.Setup(x => x.CreateAsync(It.IsAny<ProjectCreateModel>()))
             .ReturnsAsync(createdId);
