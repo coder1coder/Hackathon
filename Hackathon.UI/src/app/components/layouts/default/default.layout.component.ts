4719c6f13377c032a32ff2307724408113d32c12
Mikhail Coder <coder1coder@gmail.com>
2022-05-09 09:09:40 +0300

    изменения после ребейза
    Signed-off-by: Mikhail Coder <coder1coder@gmail.com>

--- Hackathon.UI/src/app/components/layouts/default/default.layout.component.ts
+++ Hackathon.UI/src/app/components/layouts/default/default.layout.component.ts
@@ -1,5 +1,8 @@
 import {AfterViewInit, Component, Input} from "@angular/core";
 import {AuthService} from "../../../services/auth.service";
+import {CustomDialog, ICustomDialogData} from "../../custom/custom-dialog/custom-dialog.component";
+import {RouterService} from "../../../services/router.service";
+import {MatDialog} from "@angular/material/dialog";
 
 @Component({
   selector: 'layout-default',
@@ -14,14 +17,32 @@ export class DefaultLayoutComponent implements AfterViewInit {
 
   UserName:string | undefined;
 
-  constructor(private authService:AuthService) {
-  }
+  constructor(
+    private authService:AuthService,
+    private routerService:RouterService,
+    private dialog: MatDialog
+    ) {}
 
   ngAfterViewInit(): void {
     this.authService.getCurrentUser()?.subscribe(x=>{
       if (x != null)
         this.UserName = x.fullName ?? x.userName
     })
+  }
+
+  public logout(){
+    let data: ICustomDialogData = {
+      header: 'Выход',
+      content: `Вы уверены, что хотите выйти?`,
+      acceptButtonText: `Да`
+    };
+
+    this.dialog.open(CustomDialog, { data })
+      .afterClosed()
+      .subscribe(x =>
+      {
+        if (x) this.routerService.Profile.Logout();
+      });
   }
 }
 
