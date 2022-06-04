00ca43e39bf7caed037515d7b77b283ad19ec5d8
Mikhail Coder <coder1coder@gmail.com>
2022-06-04 15:23:56 +0300

    Добавил возможность создать второй уровень в навигационном меню
    Signed-off-by: Mikhail Coder <coder1coder@gmail.com>

--- Hackathon.UI/src/app/components/nav-menu/nav-menu.component.ts
+++ Hackathon.UI/src/app/components/nav-menu/nav-menu.component.ts
@@ -16,21 +16,28 @@ export class NavMenuComponent {
     authService.getCurrentUser()?.subscribe(x=>{
       this.items = [
         new MenuItem('/events','События'),
-        new MenuItem('/users','Пользователи', x.role == UserRole.Administrator),
-        new MenuItem('/team','Команда'),
+        new MenuItem('/team','Моя команда'),
+        new MenuItem('#', 'Администрирование', x.role == UserRole.Administrator,[
+          new MenuItem('/users','Пользователи'),
+        ]),
       ].filter(x=>x.access);
     })
   }
 }
 
 class MenuItem {
+  unique: number
   routeLink:string
   text:string
   access:boolean
+  children: MenuItem[]
 
-  constructor(routeLink:string, text:string, access:boolean = true) {
+  constructor(routeLink:string, text:string, access:boolean = true, children: MenuItem[] = []) {
+    this.unique = new Date().getTime();
+
     this.routeLink = routeLink;
     this.text = text;
     this.access = access;
+    this.children = children;
   }
 }
