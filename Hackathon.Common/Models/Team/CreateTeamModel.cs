c93cc8c65d38aecab59cec20758d0784a09e3f0d
Андрей Федерягин <eniluck@mail.ru>
2022-07-09 18:18:08 +0500

    feat: для моделей поле тип команды

--- Hackathon.Common/Models/Team/CreateTeamModel.cs
+++ Hackathon.Common/Models/Team/CreateTeamModel.cs
@@ -1,9 +1,14 @@
-﻿namespace Hackathon.Common.Models.Team
+namespace Hackathon.Common.Models.Team
 {
     public class CreateTeamModel
     {
         public string Name { get; set; }
         public long? EventId { get; set; }
         public long? OwnerId { get; set; }
+
+        /// <summary>
+        /// Тип команды
+        /// </summary>
+        public TeamType Type { get; set; }
     }
 }
