120a10ebf811e70ce9038cff51b921fba6d710a6
Mikhail Coder <coder1coder@gmail.com>
2022-11-24 21:12:59 +0300

    reactions

--- Hackathon.Tests/Hackathon.Tests.Integration/User/UserControllerTests.cs
+++ Hackathon.Tests/Hackathon.Tests.Integration/User/UserControllerTests.cs
@@ -1,11 +1,8 @@
 ﻿using System.Linq;
 using System.Threading.Tasks;
 using FluentAssertions;
-using Hackathon.BL.User;
-using Hackathon.Common.Models.User;
 using Hackathon.Contracts.Requests.User;
 using Hackathon.Tests.Integration.Base;
-using Refit;
 using Xunit;
 
 namespace Hackathon.Tests.Integration.User
@@ -33,90 +30,5 @@ namespace Hackathon.Tests.Integration.User
                 options.Excluding(x=>x.Password));
         }
 
-        [Fact]
-        public async Task GetReactions_Should_ReturnArray()
-        {
-            //arrange
-            var (targetUserId, _) = await RegisterUser();
-
-            await UsersApi.AddReaction(targetUserId, UserProfileReaction.Like);
-
-            //act
-            var response = await UsersApi.GetReactions(targetUserId);
-
-            //assert
-            Assert.NotNull(response);
-            response.Should().ContainSingle(x => x == UserProfileReaction.Like);
-        }
-
-        [Fact]
-        public async Task AddReactions_Should_Success()
-        {
-            //arrange
-            var (targetUserId, _) = await RegisterUser();
-
-            //act
-            //assert
-            await FluentActions.Invoking(async () => await UsersApi.AddReaction(targetUserId, UserProfileReaction.Like))
-                .Should()
-                .NotThrowAsync();
-        }
-
-        [Fact]
-        public async Task RemoveReaction_Should_Success()
-        {
-            //arrange
-            const UserProfileReaction reaction = UserProfileReaction.Like;
-
-            var (targetUserId, _) = await RegisterUser();
-
-            await UsersApi.AddReaction(targetUserId, reaction);
-
-            //act
-            //assert
-            await FluentActions.Invoking(async () => await UsersApi.RemoveReaction(targetUserId, reaction))
-                .Should()
-                .NotThrowAsync();
-        }
-
-        [Fact]
-        public async Task AddReaction_WhenReactionAlreadyExist_Should_Throw_ValidationException()
-        {
-            //arrange
-            const UserProfileReaction reaction = UserProfileReaction.Like;
-
-            var (targetUserId, _) = await RegisterUser();
-
-            await UsersApi.AddReaction(targetUserId, reaction);
-
-            //act
-            //assert
-            await FluentActions.Invoking(async () => await UsersApi.AddReaction(targetUserId, reaction))
-                .Should()
-                .ThrowAsync<ValidationApiException>()
-                .Where(x =>
-                    x.Content != null
-                    && x.Content.Status == 400
-                    && x.Content.Detail == UserService.ReactionAlreadyExistMessage);
-        }
-
-        [Fact]
-        public async Task RemoveReaction_WhenReactionNotExist_Should_Throw_ValidationException()
-        {
-            //arrange
-            const UserProfileReaction reaction = UserProfileReaction.Like;
-
-            var (targetUserId, _) = await RegisterUser();
-
-            //act
-            //assert
-            await FluentActions.Invoking(async () => await UsersApi.RemoveReaction(targetUserId, reaction))
-                .Should()
-                .ThrowAsync<ValidationApiException>()
-                .Where(x =>
-                    x.Content != null
-                    && x.Content.Status == 400
-                    && x.Content.Detail == UserService.ReactionNotExistMessage);
-        }
     }
 }
---
+++ Hackathon.UI/src/app/models/User/UserProfileReaction.ts
@@ -0,0 +1,7 @@
+export enum UserProfileReaction
+{
+  None = 0,
+  Like = 1,
+  Heart = 2,
+  Fire = 4
+}
