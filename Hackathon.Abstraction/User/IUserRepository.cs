e8d68c095395326fcd9c6d1a413654ac3349b378
coder1coder <13835906+coder1coder@users.noreply.github.com>
2022-11-24 23:26:56 +0300

    Реакции на профиль пользователя (#214)

--- Hackathon.Abstraction/User/IUserRepository.cs
+++ Hackathon.Abstraction/User/IUserRepository.cs
@@ -56,31 +56,5 @@ namespace Hackathon.Abstraction.User
         /// <param name="profileImageId">Идентификатор картинки в хранилище</param>
         /// <returns></returns>
         Task UpdateProfileImageAsync(long userId, Guid profileImageId);
-
-        /// <summary>
-        /// Получить все реакции на профиль пользователя
-        /// </summary>
-        /// <param name="userId">Идентификатор пользователя инициировавшего реакцию</param>
-        /// <param name="targetUserId">Идентификатор пользователя получающего реакцию</param>
-        /// <returns></returns>
-        Task<UserProfileReaction[]> GetReactionsAsync(long userId, long targetUserId);
-
-        /// <summary>
-        /// Добавить реакцию на профиль пользователя
-        /// </summary>
-        /// <param name="userId">Идентификатор пользователя инициировавшего реакцию</param>
-        /// <param name="targetUserId">Идентификатор пользователя получающего реакцию</param>
-        /// <param name="reaction">Реакция</param>
-        /// <returns></returns>
-        Task AddReactionAsync(long userId, long targetUserId, UserProfileReaction reaction);
-
-        /// <summary>
-        /// Удалить реакцию на профиль пользователя
-        /// </summary>
-        /// <param name="userId">Идентификатор пользователя инициировавшего реакцию</param>
-        /// <param name="targetUserId">Идентификатор пользователя получившего реакцию</param>
-        /// <param name="reaction">Реакция</param>
-        /// <returns></returns>
-        Task RemoveReactionAsync(long userId, long targetUserId, UserProfileReaction reaction);
     }
 }
