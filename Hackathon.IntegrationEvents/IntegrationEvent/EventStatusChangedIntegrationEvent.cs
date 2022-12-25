2ec3fd0612cdc3c26c66b9441dec96705471ba4d
Mikhail Coder <coder1coder@gmail.com>
2022-12-25 23:38:52 +0300

    #248 добавить хаб в который падает ивент об изменении статуса события
    Signed-off-by: Mikhail Coder <coder1coder@gmail.com>

---
+++ Hackathon.IntegrationEvents/IntegrationEvent/EventStatusChangedIntegrationEvent.cs
@@ -0,0 +1,23 @@
+using Hackathon.Abstraction.IntegrationEvents;
+using Hackathon.Common.Models.Event;
+
+namespace Hackathon.IntegrationEvents.IntegrationEvent;
+
+public sealed class EventStatusChangedIntegrationEvent: IIntegrationEvent
+{
+    /// <summary>
+    /// Идентификатор события
+    /// </summary>
+    public long EventId { get; }
+
+    /// <summary>
+    /// Статус события
+    /// </summary>
+    public EventStatus Status { get; }
+
+    public EventStatusChangedIntegrationEvent(long eventId, EventStatus status)
+    {
+        EventId = eventId;
+        Status = status;
+    }
+}
