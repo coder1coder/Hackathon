f9bf87ed3056dc2a200f884cbfec22830a3a6972
coder1coder <13835906+coder1coder@users.noreply.github.com>
2023-02-18 15:43:19 +0300

    #264 Валидация сообщения чата (#266)

---
+++ Hackathon.BL.Validation/Chat/CreateChatMessageValidator.cs
@@ -0,0 +1,21 @@
+using FluentValidation;
+using Hackathon.Common.Models.Chat;
+
+namespace Hackathon.BL.Validation.Chat;
+
+public class CreateChatMessageValidator: AbstractValidator<ICreateChatMessage>
+{
+    public CreateChatMessageValidator()
+    {
+        RuleFor(x => x.Message)
+            .NotEmpty()
+            .WithMessage("{Property} обязательно к заполнению")
+            .MinimumLength(1)
+            .MaximumLength(200)
+            .WithMessage("{PropertyName} допускает длину от {MinLength} до {MaxLength} символов")
+            .WithName("Сообщение");
+
+        RuleFor(x => x.Options)
+            .IsInEnum();
+    }
+}
