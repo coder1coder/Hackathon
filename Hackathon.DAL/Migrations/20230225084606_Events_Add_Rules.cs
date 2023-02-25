c4c75c1c0b67ec69ee66cc932cd4ff127ab694b4
coder1coder <13835906+coder1coder@users.noreply.github.com>
2023-02-25 12:49:11 +0300

    #261 добавлено поле "Правила" для события (#271)

---
+++ Hackathon.DAL/Migrations/20230225084606_Events_Add_Rules.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Hackathon.DAL.Migrations
+{
+    public partial class Events_Add_Rules : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Rules",
+                table: "Events",
+                type: "text",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Rules",
+                table: "Events");
+        }
+    }
+}
