9a55f7178e2dcef619b6914355950dcb1f21927f
Evgenii Solovev <solovyevmeh163@gmail.com>
2023-03-04 17:26:36 +0500

    #237 Добавить настройки профиля пользователя (#272)
    Co-authored-by: Evgenii Solovev <AshtonBro@github.com>
    Co-authored-by: coder1coder <13835906+coder1coder@users.noreply.github.com>

--- Hackathon.UI/src/app/components/profile/view/profile.view.component.ts
+++ Hackathon.UI/src/app/components/profile/view/profile.view.component.ts
@@ -1,9 +1,9 @@
-import {AfterViewChecked, AfterViewInit, Component, ElementRef, ViewChild} from "@angular/core";
+import {AfterViewChecked, Component, ElementRef, OnInit, ViewChild} from "@angular/core";
 import {UserRoleTranslator} from "src/app/models/User/UserRole";
 import {AuthService} from "../../../services/auth.service";
 import {TeamService} from "../../../services/team.service";
-import {catchError, of, switchMap} from "rxjs";
-import {IUser} from "../../../models/User/IUser";
+import {catchError, of, switchMap, takeUntil} from "rxjs";
+import {IUpdateUser, IUser} from "../../../models/User/IUser";
 import {UserProfileReaction} from "src/app/models/User/UserProfileReaction";
 import {ActivatedRoute} from "@angular/router";
 import {UserService} from "../../../services/user.service";
@@ -12,35 +12,36 @@ import {SnackService} from "../../../services/snack.service";
 import {FriendshipStatus} from "../../../models/Friendship/FriendshipStatus";
 import {MatTabGroup} from "@angular/material/tabs";
 import {Team} from "../../../models/Team/Team";
-import { UserEmailStatus } from "src/app/models/User/UserEmailStatus";
+import {UserEmailStatus} from "src/app/models/User/UserEmailStatus";
+import {WithFormComponentBase} from "../../WithFormComponentBase";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {IKeyValue} from "../../../common/key-value.interface";
+import {emailRegex} from "../../../common/patterns/email-regex";
 
 @Component({
   templateUrl: './profile.view.component.html',
   styleUrls:['./profile.view.component.scss']
 })
-export class ProfileViewComponent implements AfterViewInit, AfterViewChecked {
+export class ProfileViewComponent extends WithFormComponentBase implements OnInit, AfterViewChecked {
 
-  UserRoleTranslator = UserRoleTranslator;
   @ViewChild(MatTabGroup) public friendshipTabs!: MatTabGroup;
   @ViewChild('confirmationCodeInput') confirmationCodeInput: ElementRef;
 
-  userId: number;
-  user?: IUser;
-  userTeam?: Team | null;
-  authUserId: number;
+  public form = new FormGroup({});
+  public UserRoleTranslator = UserRoleTranslator;
+  public userId: number;
+  public user: IUser;
+  public userTeam: Team | null;
+  public authUserId: number;
+  public isEditMode: boolean = false;
+  public canUploadImage: boolean = false;
+  public isLoading: boolean = true;
+  public userProfileReaction = UserProfileReaction;
+  public friendshipStatus = FriendshipStatus;
+  public userEmailStatus = UserEmailStatus;
 
-  canUploadImage: boolean = false;
-
-  isLoading: boolean = true;
-
-  userProfileReactions: UserProfileReaction = UserProfileReaction.None;
-  UserProfileReaction = UserProfileReaction;
-  FriendshipStatus = FriendshipStatus;
-  UserEmailStatus = UserEmailStatus;
-
-  Object = Object;
-
-  availableReactions: string[] = [];
+  private userProfileReactions: UserProfileReaction = UserProfileReaction.None;
+  private emailRegexp: RegExp = emailRegex;
 
   constructor(
     private authService: AuthService,
@@ -49,48 +50,130 @@ export class ProfileViewComponent implements AfterViewInit, AfterViewChecked {
     private userService: UserService,
     private userProfileReactionService: UserProfileReactionService,
     private snackService: SnackService,
+    private fb: FormBuilder
   ) {
-
+    super();
     this.userService = userService;
     this.snackService = snackService;
-
     this.authUserId = this.authService.getUserId() ?? 0;
     this.userId = this.activateRoute.snapshot.params['userId'] ?? this.authUserId;
   }
 
-  public ngAfterViewChecked(): void {
+  ngOnInit(): void {
+    this.fetchData();
+    this.initForm();
+  }
 
-    if (this.userId == this.authUserId)
-    {
+  public ngAfterViewChecked(): void {
+    if (this.userId == this.authUserId) {
       setTimeout(() =>{
         this.friendshipTabs?.realignInkBar()
       }, 0);
     }
-
   }
 
-  ngAfterViewInit(): void {
-    this.fetchData();
+  public userEdit(): void {
+    this.isEditMode = true;
+    if (Boolean(this.user)) {
+      this.form.setValue(this.mapUserValue(this.user));
+    }
   }
 
-  private fetchData(): void{
+  public saveUserEdit(): void {
+    const request: IUpdateUser = {
+      id: this.userId,
+      ...this.form.getRawValue()
+    };
+    if (JSON.stringify(request) !== JSON.stringify(this.mapUserValue(this.user))) {
+      this.userService.updateUser(request)
+        .pipe(takeUntil(this.destroy$))
+        .subscribe({
+          next: () => {
+            this.isEditMode = false;
+            this.snackService.open("Данные профиля изменены");
+            this.fetchData();
+          },
+          error: (error) => {
+            const errorDetail = error?.error?.detail;
+            if (errorDetail) {
+              this.snackService.open(errorDetail);
+            } else {
+              this.snackService.open("Не удалось обновить профиль");
+            }
+          }
+        });
+    } else {
+      this.isEditMode = false;
+    }
+  }
 
+  public userEditCancel(): void {
+    this.isEditMode = false;
+    this.form.reset();
+  }
+
+  public toggleReaction(event: Event, reaction: UserProfileReaction):void {
+    this.userProfileReactionService.toggleReaction(this.userId, reaction)
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((_)=>{
+        this.fetchReactions();
+      });
+  }
+
+  public getAvailableUserProfileReactions(): number[] {
+    return Object.keys(UserProfileReaction)
+      .filter(x=>!isNaN(Number(x)))
+      .map(s=>Number(s))
+      .filter(x=>x !== UserProfileReaction.None);
+  }
+
+  public isReactionActive(reaction: UserProfileReaction): boolean {
+    return (this.userProfileReactions & reaction) === reaction;
+  }
+
+  public confirmEmail(): void {
+    const code = this.confirmationCodeInput?.nativeElement?.value ?? '';
+    this.userService.confirmEmail(code)
+      .pipe(takeUntil(this.destroy$))
+      .subscribe({
+        next: () => this.fetchData(),
+        error: () => this.snackService.open("Ошибка подтверждения Email")
+      })
+  }
+
+  public createEmailConfirmationRequest() {
+    this.userService.createEmailConfirmationRequest()
+      .pipe(takeUntil(this.destroy$))
+      .subscribe({
+        next: () => this.fetchData(),
+        error: () => this.snackService.open("Ошибка отправки запроса на подтверждение Email")
+      });
+  }
+
+  private fetchReactions(): void {
+    this.userProfileReactionService.get(this.userId)
+      .pipe(takeUntil(this.destroy$))
+      .subscribe({
+        next: (r: UserProfileReaction) => {
+          this.userProfileReactions = r;
+        },
+        error: () => {}
+      });
+  }
+
+  private fetchData(): void {
     this.isLoading = true;
     this.userService.getById(this.userId)
       ?.pipe(
+        takeUntil(this.destroy$),
         switchMap((user)=> {
-
-          let currentUserId = this.authService.getUserId();
-
-          this.canUploadImage = currentUserId == this.userId;
-
+          const currentUserId = this.authService.getUserId();
+          this.canUploadImage = currentUserId === this.userId;
           this.user = user;
 
-          if (this.userId != this.authUserId)
-          {
+          if (this.userId != this.authUserId) {
             this.fetchReactions();
           }
-
           return this.teamService.getMyTeam()
         }),
         catchError(() => {
@@ -101,63 +184,19 @@ export class ProfileViewComponent implements AfterViewInit, AfterViewChecked {
         this.userTeam = res
       }
     )
-
   }
 
-  public toggleReaction(event: Event, reaction: UserProfileReaction):void {
-    this.userProfileReactionService.toggleReaction(this.userId, reaction)
-      .subscribe((_)=>{
-        this.fetchReactions();
-      });
+  private initForm(): void {
+    this.form = this.fb.group({
+      fullName: [null, [Validators.required, Validators.minLength(2), Validators.maxLength(100)]],
+      email: [null, [Validators.required, Validators.pattern(this.emailRegexp)]]
+    })
   }
 
-  public getAvailableUserProfileReactions():number[]
-  {
-    return Object.keys(UserProfileReaction)
-      .filter(x=>!isNaN(Number(x)))
-      .map(s=>Number(s))
-      .filter(x=>x !== UserProfileReaction.None);
+  private mapUserValue(user: IUser): IKeyValue {
+    return {
+      fullName: user.fullName ? user.fullName : null,
+      email: user.email?.address ? user.email.address : null,
+    }
   }
-
-  public isReactionActive(reaction: UserProfileReaction):boolean {
-    return (this.userProfileReactions & reaction) === reaction;
-  }
-
-  private fetchReactions() {
-    this.userProfileReactionService.get(this.userId)
-      .subscribe({
-        next: (r: UserProfileReaction) => {
-          this.userProfileReactions = r;
-        },
-        error: _ => {
-        }
-      });
-  }
-
-  public confirmEmail():void{
-    let code = this.confirmationCodeInput?.nativeElement?.value ?? '';
-    this.userService.confirmEmail(code)
-      .subscribe({
-        next: () => {
-          this.fetchData();
-        },
-        error: _ => {
-          this.snackService.open("Ошибка подтверждения Email")
-        }
-      })
-  }
-
-  public createEmailConfirmationRequest()
-  {
-    this.userService.createEmailConfirmationRequest()
-      .subscribe({
-        next: () =>{
-          this.fetchData();
-        },
-        error: _ => {
-          this.snackService.open("Ошибка отправки запроса на подтверждение Email");
-        }
-      });
-  }
-
 }
