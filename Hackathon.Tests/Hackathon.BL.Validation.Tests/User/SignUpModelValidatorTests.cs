8ca3ebe4f619a4ada2ded37e1bc55168faa7b219
coder1coder <13835906+coder1coder@users.noreply.github.com>
2023-03-21 11:51:41 +0300

    #277 Добавил ограничения на имена пользователей. Список запрещенных к указанию именам вынес в конфиг (#280)

---
+++ Hackathon.Tests/Hackathon.BL.Validation.Tests/User/SignUpModelValidatorTests.cs
@@ -0,0 +1,58 @@
+using FluentValidation.TestHelper;
+using Hackathon.BL.Validation.User;
+using Hackathon.Common.Abstraction.User;
+using Hackathon.Common.Configuration;
+using Hackathon.Common.Models;
+using Hackathon.Common.Models.Base;
+using Hackathon.Common.Models.User;
+using Microsoft.Extensions.Options;
+using Moq;
+using Xunit;
+
+namespace Hackathon.BL.Validation.Tests.User;
+
+public class SignUpModelValidatorTests
+{
+    private readonly SignUpModelValidator _validator;
+
+    private readonly Mock<IUserRepository> _userRepositoryMock = new();
+    private readonly RestrictedNames _restrictedNames;
+
+    public SignUpModelValidatorTests()
+    {
+        _restrictedNames = new RestrictedNames
+        {
+            Users = new[] {"administrator", "admin"}
+        };
+
+        _validator = new SignUpModelValidator(_userRepositoryMock.Object, new OptionsWrapper<RestrictedNames>(_restrictedNames));
+    }
+
+    [Fact]
+    public void Should_Fail_When_Specify_UserName_From_ListOfRestrictions()
+    {
+        //arrange
+        var signUpModel = new SignUpModel
+        {
+            Email = "test@test.ru",
+            UserName = _restrictedNames.Users.First()
+        };
+
+        _userRepositoryMock.Setup(x => x
+                .GetAsync(It.Is<GetListParameters<UserFilter>>(s =>
+                    s.Filter != null && s.Filter.Email == signUpModel.Email)))
+            .ReturnsAsync(new BaseCollection<UserModel>
+            {
+                Items = ArraySegment<UserModel>.Empty,
+                TotalCount = 0
+            });
+
+        //act
+        //assert
+        _validator.TestValidate(signUpModel)
+            .ShouldHaveValidationErrorFor(x => x.UserName)
+            .WithErrorMessage(SignUpModelValidator.SpecifyUserNameIsRestricted);
+    }
+
+
+}
