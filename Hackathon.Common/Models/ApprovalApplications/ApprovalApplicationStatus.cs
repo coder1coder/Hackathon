f9532f847e725f32a8f91d481aaa3ef4f83c6a02
coder1coder <13835906+coder1coder@users.noreply.github.com>
2023-08-30 19:25:57 +0300

    342: добавил сущность заявлений согласования, статус модерации мероприятия (#350)

---
+++ Hackathon.Common/Models/ApprovalApplications/ApprovalApplicationStatus.cs
@@ -0,0 +1,22 @@
+﻿namespace Hackathon.Common.Models.ApprovalApplications;
+
+/// <summary>
+/// Статус согласования
+/// </summary>
+public enum ApprovalApplicationStatus: byte
+{
+    /// <summary>
+    /// На согласовании
+    /// </summary>
+    Requested = 0,
+
+    /// <summary>
+    /// Согласовано
+    /// </summary>
+    Approved = 1,
+
+    /// <summary>
+    /// Отклонено
+    /// </summary>
+    Rejected = 2
+}
---
+++ Hackathon.DAL/Configurations/ApprovalApplications/ApprovalApplicationConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Hackathon.DAL.Entities.ApprovalApplications;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Hackathon.DAL.Configurations.ApprovalApplications;
+
+public class ApprovalApplicationConfiguration: IEntityTypeConfiguration<ApprovalApplication>
+{
+    public void Configure(EntityTypeBuilder<ApprovalApplication> builder)
+    {
+        builder.ToTable("ApprovalApplications");
+
+        builder.Property(x => x.Comment)
+            .HasMaxLength(300);
+
+        builder.HasOne(x => x.Author)
+            .WithMany();
+
+        builder.HasOne(x => x.Event)
+            .WithOne(x => x.ApprovalApplication)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
