a70274b5032fff23932dc76010e790198cfb6591
Mikhail Coder <coder1coder@gmail.com>
2023-12-24 23:41:23 +0300

    Добавил пропущенную миграцию

---
+++ Hackathon.DAL/Migrations/20231224204052_MembersTeams_ChangeRoleType.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Hackathon.DAL.Migrations
+{
+    public partial class MembersTeams_ChangeRoleType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<byte>(
+                name: "Role",
+                table: "MembersTeams",
+                type: "smallint",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Role",
+                table: "MembersTeams",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(byte),
+                oldType: "smallint");
+        }
+    }
+}
